71629f099f19f2d0affab460aab5fff8bcc66b9d
Pythonsway <adam@pythonsway.it>
2020-10-21 22:04:29 +0200

    new Kaper

--- jpk.py
+++ jpk.py
@@ -1,15 +1,15 @@
-import os
-import glob
-import re
 import csv
+import glob
+import os
+import re
 import time
 
 kaper = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'kaper'))
 schemat = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', 'schemat'))
 plikiKaper0 = glob.glob(kaper + '\\*.txt')
-plikiKaper = [[plikiKaper0[0], 13, -6, [1, 8, 6, 7, 4, 2, 3, 12, 13]],
-              [plikiKaper0[1], 17, -6, [1, 9, 6, 7, 4, 2, 3, 13, 14]]]
+plikiKaper = [[plikiKaper0[0], 13, -6, [1, 9, 7, 8, 4, 2, 3, 13, 14]],
+              [plikiKaper0[1], 17, -6, [1, 10, 7, 8, 4, 2, 3, 14, 15]]]
 plikSchemat = glob.glob(schemat + '\\*.csv')
 
 
@@ -19,12 +19,12 @@ def main():
         with open(plik[0], 'r') as in_text, open(plik[0] + '.csv', 'w', newline='') as out_csv:
             in_reader = list(csv.reader(in_text, delimiter='|'))
             out_writer = csv.writer(out_csv, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-            rNip = re.compile(r'^\w:')
+            rNip = re.compile(r'(\d{3})(?:-)(\d{2,3})(?:-)(\d{2})(?:-)(\d{2,3})')
             rKs = re.compile(r'(\d+)\s(\d+)')
             for row in in_reader[plik[1]:plik[2]]:
                 stripped = [i.strip() for i in row]
                 reordered = [stripped[i] for i in plik[3]]
-                noNip = [rNip.sub('', i) for i in reordered]
+                noNip = [rNip.sub(r'\1\2\3\4', i) for i in reordered]
                 noKs = [rKs.sub(r'\1\2', i) for i in noNip]
                 brak = ['BRAK' if not i else i for i in noKs]
                 out_writer.writerow(brak)
